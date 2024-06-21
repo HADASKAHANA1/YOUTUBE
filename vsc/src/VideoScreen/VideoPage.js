f15b27c29c36ecd868fb8d9690cae96e90a69bca
HADASKAHANA1 <amitaikahana53@gmail.com>
2024-06-21 15:02:27 +0300

    UPDATE BUTTON

--- vsc/src/VideoScreen/VideoPage.js
+++ vsc/src/VideoScreen/VideoPage.js
@@ -40,14 +40,17 @@ function VideoPage() {
 
   return (
     <div className="video-page">
+      {/* Link to Home Page */}
       <Link to="/" className="home-link">
         <img src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg" alt="YouTube Logo" className="youtube-logo" />
       </Link>
+
       <div className="video-details">
-        <h2>{currentVideo.title}</h2>
         <video controls src={currentVideo.url} className="video-player" ref={videoRef} />
+        <h2>{currentVideo.title}</h2>
         <p>{currentVideo.description}</p>
       </div>
+
       <div className="video-list">
         {otherVideos.map((vid) => (
           <Link key={vid.id} to={`/videos/${vid.id}`} className="video-item-link">
